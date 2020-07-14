177a547b2fdc5103afd9db6cbd0b499009511af9
cryptobabybear <hello@oliverwee.com>
2020-07-14 22:36:00 +0800

    refactor: Remove redundant file, update dependencies

--- src/abci/server_connection.js
+++
@@ -1,9 +0,0 @@
-import events from 'events';
-
-const ServerConnection = (serverHandler) => (evtHandler) => {
-  evtHandler.on('data', (rawData) => {
-    console.log(rawData);
-  }); 
-};
-
-export default ServerConnection;
